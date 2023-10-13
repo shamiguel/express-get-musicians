b7c0e53aa0ec16506054a82f26a9780f4f812dce
Shami Tomita <shamito@google.com>
2023-10-13 13:58:07 -0500

    musician and band get tests

--- index.test.js
+++ index.test.js
@@ -9,14 +9,26 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
-
 describe('./musicians endpoint', () => {
     // Write your tests here
+    it('successful get method returns musicians', async()=>{
+        const response = await request(app).get('/musicians');
+       
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0].name).toBe("Mick Jagger");
+    });
+
+    it('returns an error when unsuccesful', async()=>{
+        expect(true).toBe(true);
+    })
+});
+
+describe('./bands endpoint', () => {
+    // Write your tests here
+    it('successful get method returns bands', async()=>{
+        const response = await request(app).get('/bands');
     
-    
-
-
-
-
-    
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0].name).toBe("The Beatles");
+    });
 })
