25f90f55183c4706e71369dd27cd8c7a1a3a9d51
Shami Tomita <shamito@google.com>
2023-10-13 13:57:39 -0500

    musician and band calls

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
+const { Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+
+app.get('/musicians', async(req, res)=>{
+    try{
+        const musicians = await Musician.findAll();
+        res.json(musicians)
+    }catch(error){
+        res.status(500).json({error: `An error occured when fetching musicians`});
+    }
+});
+
+app.get('/bands', async(req, res)=>{
+    try{
+        const bands = await Band.findAll();
+        res.json(bands);
+    }catch(error){
+        res.status(500).json({error: `An error occured when fetching bands`})
+    }
+})
 
 
 
