455918c25a5f60b512a2980ff8a9631bb2576a79
Shami Tomita <shamito@google.com>
2023-10-17 16:47:45 -0400

    route for get bands/:id

--- routes/bands.js
+++ routes/bands.js
@@ -11,4 +11,14 @@ router.get('/', async(req, res)=>{
     }
 })
 
+router.get('/:id', async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const bandWithMusicians = await Band.findByPk(id, {include: Musician}) ;
+        res.json(bandWithMusicians);
+    }catch(err){
+        console.log(err)
+    }
+})
+
 module.exports = router;
